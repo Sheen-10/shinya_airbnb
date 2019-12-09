38e8fd3d4127038d5cf9b8451173196f9ecea3f6
Shinya Mori <shinya199905102525@gmail.com>
2019-12-09 18:26:04 +0800

    Created rooms

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
   controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+
+  resources :rooms, except: [:edit] do
+    member do
+      get 'listing'
+      get 'pricing'
+      get 'description'
+      get 'photo_upload'
+      get 'amenities'
+      get 'location'
+    end
+  end
 end
