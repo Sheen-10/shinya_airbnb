f37e23776dd1e68547fe7c2644641ae26a28fc77
Shinya Mori <shinya199905102525@gmail.com>
2019-12-18 09:16:22 +0800

    Created reservation validation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,18 +2,23 @@ class ReservationsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+
     room = Room.find(params[:room_id])
-    start_date = Date.parse(reservation_params[:start_date])
-    end_date = Date.parse(reservation_params[:end_date])
-    days = (end_date - start_date).to_i ; 1
+    if current_user == room.user
+      flash[:alert] = "You cannot book your own property!"
+    else
+      start_date = Date.parse(reservation_params[:start_date])
+      end_date = Date.parse(reservation_params[:end_date])
+      days = (end_date - start_date).to_i ; 1
 
-    @reservation = current_user.reservations.build(reservation_params)
-    @reservation.room = room
-    @reservation.price = room.price
-    @reservation.total = room.price * days
-    @reservation.save
+      @reservation = current_user.reservations.build(reservation_params)
+      @reservation.room = room
+      @reservation.price = room.price
+      @reservation.total = room.price * days
+      @reservation.save
 
-    flash[:notice] = "Booked Successfuly..."
+      flash[:notice] = "Booked Successfuly..."
+    end
     redirect_to room
   end
 
