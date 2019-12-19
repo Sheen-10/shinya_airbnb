d75d0a9e6f72dd65caa9b3a856d5d94cac23ad24
Shinya Mori <56685893+Sheen-10@users.noreply.github.com>
2019-12-19 11:00:47 +0800

    Created reservation trips (#22)
    * Created reservation trips
    
    * Fixed reservation-trips

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create!(
+  fullname: "Johnedel Mapa",
+  email: "johnedel.mapa@sun-asterisk.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+user.skip_confirmation!
+user.save
+
+user1 = User.create!(
+  fullname: "Shinya Mori",
+  email: "shinya199905102525@gmail.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+user1.skip_confirmation!
+user1.save
+
+
+50.times do |n|
+  users = User.new(
+  fullname: Faker::Name.name,
+  email: Faker::Internet.unique.email,
+  password: "password",
+  password_confirmation: "password"
+)
+users.skip_confirmation!
+users.save!
+end
